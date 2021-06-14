845c52a8250d4e91974b4fe77b0336eb1b45ee9f
Tunde Adepegba <tundeshield@gmail.com>
2021-06-14 03:17:55 +0100

    Login requirement info update

--- src/components/Login.js
+++ src/components/Login.js
@@ -2,47 +2,57 @@ import React, { useState } from "react";
 import "../styles/Login.css";
 import bg from "../styles/images/linkedin-bg.png";
 import { auth } from "../firebase";
-import {useDispatch} from "react-redux";
-import {login} from "../features/userSlice"
+import { useDispatch } from "react-redux";
+import { login } from "../features/userSlice";
 
 function Login() {
 	const [email, setEmail] = useState("");
 	const [name, setName] = useState("");
 	const [password, setPassword] = useState("");
 	const [profilePic, setProfilePic] = useState("");
-    
-    const dispatch = useDispatch()
 
+	const dispatch = useDispatch();
 
 	const register = () => {
-        if (!name){
-            return alert("Please enter a full name.")
-        }
-        auth.createUserWithEmailAndPassword(email,password).then((userAuth)=>{
-            userAuth.user.updateProfile({
-                displayName:name,
-                photoURL:profilePic,
-            }).then(()=>{
-                dispatch(login({
-                    email:userAuth.user.email,
-                    uid: userAuth.user.uid,
-                    displayName: name,
-                    photoUrl:profilePic,
-                }))
-            })
-         }).catch(error=>alert(error))
-    };
+		if (!name) {
+			return alert("Please enter a Test Fullname, email and a password.");
+		}
+		auth
+			.createUserWithEmailAndPassword(email, password)
+			.then((userAuth) => {
+				userAuth.user
+					.updateProfile({
+						displayName: name,
+						photoURL: profilePic,
+					})
+					.then(() => {
+						dispatch(
+							login({
+								email: userAuth.user.email,
+								uid: userAuth.user.uid,
+								displayName: name,
+								photoUrl: profilePic,
+							})
+						);
+					});
+			})
+			.catch((error) => alert(error));
+	};
 	const loginToApp = (e) => {
-        e.preventDefault();
-        auth.signInWithEmailAndPassword(email, password).then(userAuth =>{
-            dispatch(login({
-                email:userAuth.user.email, 
-                uid: userAuth.user.uid,
-                displayName: userAuth.user.displayName,
-                profileUrl: userAuth.user.photoURL,
-            }))
-        } ).catch((error)=>alert (error));
-	
+		e.preventDefault();
+		auth
+			.signInWithEmailAndPassword(email, password)
+			.then((userAuth) => {
+				dispatch(
+					login({
+						email: userAuth.user.email,
+						uid: userAuth.user.uid,
+						displayName: userAuth.user.displayName,
+						profileUrl: userAuth.user.photoURL,
+					})
+				);
+			})
+			.catch((error) => alert(error));
 	};
 
 	return (
@@ -52,7 +62,7 @@ function Login() {
 				<input
 					type="text"
 					value={name}
-			 		onChange={(e) => setName(e.target.value)}
+					onChange={(e) => setName(e.target.value)}
 					placeholder="Full name is required to register"
 				/>
 				<input
